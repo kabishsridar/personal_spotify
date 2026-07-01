ee0d4c0fdeb5890a2f0efe44cc900a685dee1003
kabishsridar <kabishsridar@example.com>
2026-07-01 10:29:17 +0530

    Reorganize workspace, implement search infinite scroll, and clean fullscreen video synchronization

--- 45_mob/app/src/main/java/com/spotifyclone/models/Track.kt
+++
@@ -1,9 +0,0 @@
-package com.spotifyclone.models
-
-data class Track(
-    val id: String,
-    val title: String,
-    val artist: String,
-    val albumCoverUrl: String,
-    val durationMs: Long
-)
--- android/app/src/main/java/com/kabish/spotify/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.kabish.spotify;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
